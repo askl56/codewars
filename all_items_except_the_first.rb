1fcf8823e48ced7bcc1555f6d6810764f907ee7c
askl56 <amscott@openmailbox.org>
2015-11-09 00:17:41 +0000

    All items except the first

---
+++ all_items_except_the_first.rb
@@ -0,0 +1,7 @@
+# Description
+
+# Create a method drop that accepts a list, and a number n, and returns a copy of the list that skips the first n elements.
+
+def drop(list, n)
+  list.drop(n)
+end
