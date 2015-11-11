422d489d187e8d489ace6bc55509778b8a9dacb7
askl56 <amscott@openmailbox.org>
2015-11-11 12:34:22 +0000

    More solutions

---
+++ array_exchange.rb
@@ -0,0 +1,21 @@
+# Description
+
+# Array Exchange and Reversing
+
+# It's time for some array exchange! The objective is simple: To exchange between two arrays but with one catch; the content of the exchanged array must be reversed.
+
+# my_array = ['a', 'b', 'c']
+# other_array= [1, 2, 3]
+# my_array.exchange_with!(other_array)
+# The expected output:
+
+# my_array = [3, 2, 1]
+# other_array = ['c', 'b', 'a']
+
+class Array
+  def exchange_with!(other_array)
+    temp = self.dup
+    self.replace other_array.reverse
+    other_array.replace temp.reverse
+  end
+end
--- count_vowels_in_string.rb
+++ count_vowels_in_string.rb
@@ -7,5 +7,16 @@
 # countVowels("abcdefg") => 2 Ignore the case. return nil for non-string inputs countVowels(12) => nil
 
 def countVowels(str='')
-  str.scan(/[aeoui]/).count
+  num_vowels = 0
+
+  i = 0
+  while i < str.length
+    if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u")
+      num_vowels += 1
+    end
+
+    i += 1
+  end
+
+  return num_vowels
 end
