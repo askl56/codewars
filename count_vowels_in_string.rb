3eb174c8316c6da6af7f0728a2c2b7212be91c23
askl56 <amscott@openmailbox.org>
2015-11-12 15:27:57 +0000

    condensed count vowels solution

--- count_vowels_in_string.rb
+++ count_vowels_in_string.rb
@@ -7,16 +7,5 @@
 # countVowels("abcdefg") => 2 Ignore the case. return nil for non-string inputs countVowels(12) => nil
 
 def countVowels(str='')
-  num_vowels = 0
-
-  i = 0
-  while i < str.length
-    if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u")
-      num_vowels += 1
-    end
-
-    i += 1
-  end
-
-  return num_vowels
+  str.scan(/[aeiou]/i).size
 end
