cfd80ed6aae966898fbfd4e680661bab2634c14e
Andrew Scott <amscott@openmailbox.org>
2015-12-13 17:01:21 +0000

    Divisible by four

---
+++ divisible_by_four.rb
@@ -0,0 +1,8 @@
+# # Description
+
+# # Create a function that returns True if a given number is divisible
+# by 4, otherwise it should return False.
+
+def divisibleByFour(num)
+  num % 4 == 0 ? true : false
+end
